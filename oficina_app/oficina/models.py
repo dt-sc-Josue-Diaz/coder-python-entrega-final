d51002394e3d4d908d17448c0b1b512a530cb214
Josué Díaz <97265914+dt-sc-Josue-Diaz@users.noreply.github.com>
2025-03-17 01:26:45 -0600

    feature:Creación de modelos: Empleado, Proyecto y Tarea. Los cuales son la actividaes básicas de la gerencia.

--- oficina_app/oficina/models.py
+++ oficina_app/oficina/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Empleado(models.Model):
+    nombre = models.CharField(max_length=100)
+    puesto = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_inicio = models.DateField()
+    fecha_fin = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_asignacion = models.DateTimeField(auto_now_add=True)
+    fecha_vencimiento = models.DateField()
+    proyecto = models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
