58f90524877d34e18c690f11ac00decc1fa6e6c8
Josué Díaz <97265914+dt-sc-Josue-Diaz@users.noreply.github.com>
2025-04-13 16:26:08 -0600

    Delete oficina_app directory
    Problemas con un archivo de drive

--- oficina_app/oficina/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Empleado, Tarea, Reporte
-
-admin.site.register(Empleado)
-admin.site.register(Tarea)
-admin.site.register(Reporte)
--- oficina_app/oficina/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OficinaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'oficina'
